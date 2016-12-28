72e0663dd2a64d1fdb3b0c8657e0127314c1e354
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-12-28 22:27:21 +1100

    Can handle products! \o/

--- src/Data/Transmogrify.hs
+++ src/Data/Transmogrify.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, FlexibleContexts, FlexibleInstances,
+{-# LANGUAGE ConstraintKinds, DataKinds, FlexibleContexts, FlexibleInstances,
              MultiParamTypeClasses, ScopedTypeVariables, TypeApplications,
              TypeFamilies, TypeOperators, UndecidableInstances #-}
 
@@ -14,6 +14,7 @@
  -}
 module Data.Transmogrify where
 
+import Control.Arrow (first)
 import Data.Int
 import Data.Proxy
 import Data.Word
@@ -110,3 +111,100 @@ instance (RawShape c) => GRawShape (K1 i c) where
   gToRaw = toRaw . unK1
 
   gFromRaw = K1 . fromRaw
+
+instance (GRawProduct f, GRawShape g) => GRawShape (f :*: g) where
+
+  type GRaw (f :*: g) = GPRaw f (GRaw g)
+
+  gToRaw (a :*: b) = gPToRaw a (gToRaw b)
+
+  gFromRaw r = let (a, r') = gPFromRaw r
+               in a :*: gFromRaw r'
+
+--------------------------------------------------------------------------------
+
+class GRawProduct f where
+
+  type GPRaw f (r :: * -> *) :: * -> *
+
+  gPToRaw :: f x -> r x -> GPRaw f r x
+
+  gPFromRaw :: GPRaw f r x -> (f x, r x)
+
+instance (GRawProduct f, GRawProduct g) => GRawProduct (f :*: g) where
+
+  type GPRaw (f :*: g) r = GPRaw f (GPRaw g r)
+
+  gPToRaw (f :*: g) r = gPToRaw f (gPToRaw g r)
+
+  gPFromRaw r = let (f, r') = gPFromRaw r
+                in first (f :*:) (gPFromRaw r')
+
+-- Technically does'nt apply to Datatypes, but having two repeated
+-- instances for Constructors and Selectors would just be redundant.
+instance (GRawProduct f) => GRawProduct (M1 i t f) where
+
+  type GPRaw (M1 i t f) r = GPRaw f r
+
+  gPToRaw = gPToRaw . unM1
+
+  gPFromRaw = first M1 . gPFromRaw
+
+instance (RawProdEnd c) => GRawProduct (K1 i c) where
+
+  type GPRaw (K1 i c) r = GPERaw (Raw c) r
+
+  gPToRaw = peToRaw . unK1
+
+  gPFromRaw = first K1 . peFromRaw
+
+-- | How to handle a value contained within a product; we want to
+--   smush products within this overall outer one.
+class GRawProdEnd' (f :: * -> *) (b :: Bool) where
+
+  type GPERaw' f b (r :: * -> *) :: * -> *
+
+  gPEToRaw' :: Proxy b -> f x -> r x -> GPERaw' f b r x
+
+  gPEFromRaw' :: Proxy b -> GPERaw' f b r x -> (f x, r x)
+
+type family IsProduct (f :: * -> *) :: Bool where
+  IsProduct (f :*: g) = 'True
+  IsProduct f         = 'False
+
+instance (GRawProduct f, GRawProduct g) => GRawProdEnd' (f :*: g) 'True where
+
+  type GPERaw' (f :*: g) 'True r = GPRaw (f :*: g) r
+
+  gPEToRaw' _ = gPToRaw
+
+  gPEFromRaw' _ = gPFromRaw
+
+instance (GRawShape f) => GRawProdEnd' f 'False where
+
+  type GPERaw' f 'False r = (GRaw f) :*: r
+
+  gPEToRaw' _ f r = gToRaw f :*: r
+
+  gPEFromRaw' _ (f :*: r) = (gFromRaw f, r)
+
+type RawProdEnd a = (RawShape a, GRawProdEnd (Raw a))
+
+class (GRawProdEnd' f (IsProduct f)) => GRawProdEnd f
+instance (GRawProdEnd' f (IsProduct f)) => GRawProdEnd f
+
+type GPERaw f r = GPERaw' f (IsProduct f) r
+
+type PERaw a r = GPERaw (Raw a) r
+
+gPEToRaw :: forall f r x. (GRawProdEnd f) => f x -> r x -> GPERaw f r x
+gPEToRaw = gPEToRaw' (Proxy @(IsProduct f))
+
+peToRaw :: (RawProdEnd a) => a -> r x -> PERaw a r x
+peToRaw = gPEToRaw . toRaw
+
+gPEFromRaw :: forall f r x. (GRawProdEnd f) => GPERaw f r x -> (f x, r x)
+gPEFromRaw = gPEFromRaw' (Proxy @(IsProduct f))
+
+peFromRaw :: (RawProdEnd a) => PERaw a r x -> (a, r x)
+peFromRaw = first fromRaw . gPEFromRaw
